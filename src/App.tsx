7ea42265738d71480f661c1500af64d89e4d8b3b
Vasily <vasa-svistun@mail.ru>
2021-10-16 18:43:10 +0300

    start to implement rules for svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { FC } from "react";
+import "./App.scss";
+import Img from "../public/images/example.jpg";
 
 export const App: FC = () => {
-  return <div>App component</div>;
+  return (
+    <div>
+      App component
+      <img className="img" src={Img} alt="" />
+    </div>
+  );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,42 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-  mode: "development",
 };
