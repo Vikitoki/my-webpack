c4f2bea690096209c11f0671d884537f61452320
Vasily <vasa-svistun@mail.ru>
2021-10-16 15:08:12 +0300

    start to setting webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  mode: "development",
+};
